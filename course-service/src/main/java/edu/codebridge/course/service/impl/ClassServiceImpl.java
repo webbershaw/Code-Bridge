71b8aa365c1cae6a6eee09bce65cb6cd1778038e
李凯贤 <2068788528@qq.com>
2023-04-03 21:35:44 +0800

    纠错

--- course-service/src/main/java/edu/codebridge/course/service/impl/ClassServiceImpl.java
+++ course-service/src/main/java/edu/codebridge/course/service/impl/ClassServiceImpl.java
@@ -45,14 +45,17 @@ public class ClassServiceImpl implements ClassService {
      */
     @Override
     public Result addClass(Class clazz,HttpServletRequest request) {
+        //校验登录
         if (Check.checkUser(request).getData()==null){
            return Check.checkUser(request);
         }
         User user =(User)Check.checkUser(request).getData();
 
+        //添加需要教师权限
         if(!user.getIdentity().equals(IdentityCode.TEACHER)){
             return new Result(ErrorCode.PERMISSION_DENIED,null,"您的权限不足" );
         }
+        //添加分班
         classMapper.addClass(clazz);
         return new Result(ErrorCode.OK,null,"新增班级成功！");
     }
@@ -65,20 +68,24 @@ public class ClassServiceImpl implements ClassService {
      */
     @Override
     public Result updateClassById(Class clazz, HttpServletRequest request) {
+        //校验登录
         if (Check.checkUser(request).getData()==null){
             return Check.checkUser(request);
         }
         User user =(User)Check.checkUser(request).getData();
+        //修改需要教师权限
         if(!user.getIdentity().equals(IdentityCode.TEACHER)){
             return new Result(ErrorCode.PERMISSION_DENIED,null,"您的权限不足" );
         }
+
+        //只能操作自己的班级
         Long id = user.getId();
         if (clazz.getUserId()!=id){
             return new Result(ErrorCode.ERR,null,"此为他人的班级，无法修改！");
         }
-
+        //修改自己的班级
         classMapper.updateClassById(clazz);
-        return new Result(ErrorCode.OK,null,"修改成功！");
+        return new Result(ErrorCode.OK,null,"修改班级成功！");
 
     }
 
@@ -90,44 +97,62 @@ public class ClassServiceImpl implements ClassService {
      */
     @Override
     public Result queryUserIdByClassId(Integer classId, HttpServletRequest request) {
+        //校验登录
         if (Check.checkUser(request).getData()==null){
             return Check.checkUser(request);
         }
         User user =(User)Check.checkUser(request).getData();
+
+        //查询
         Long userId = classMapper.queryUserIdByClassId(classId);
         return new  Result(ErrorCode.OK,userId,"查询成功！");
     }
 
+    /**
+     * 通过clssIds得到userIds
+     * @param classIds
+     * @param request
+     * @return
+     */
     @Override
     public Result queryUserIdsByClassIds(List<Integer> classIds, HttpServletRequest request) {
+        //登录校验
         if (Check.checkUser(request).getData()==null){
             return Check.checkUser(request);
         }
         User user =(User)Check.checkUser(request).getData();
+        //查询
         List<Long> userIds = classMapper.queryUerIdsByClassIds(classIds);
         return new Result(ErrorCode.OK,userIds,"查询成功！");
     }
 
+    /**
+     * 通过classId得到class
+     * @param classId
+     * @param request
+     * @return
+     */
     @Override
     public Result queryClassByClassId(Integer classId, HttpServletRequest request) {
+        //登录校验
         if (Check.checkUser(request).getData()==null){
             return Check.checkUser(request);
         }
         User user =(User)Check.checkUser(request).getData();
-
+        //把用户敏感信息去掉
         Class clazz = classMapper.getClassByIdAndNoDeleted(classId);
         User user2 = userClient.queryById(clazz.getUserId());
-        clazz.setUser(user2);
         user2.setPwd(null);
         user2.setEmail(null);
         user2.setTel(null);
+        clazz.setUser(user2);
         User user3 = userClient.queryById(clazz.getCourse().getUserId());
         user3.setPwd(null);
         user3.setTel(null);
         user3.setEmail(null);
         clazz.getCourse().setUser(user3);
 
-
+        //这个课程的学生和这个分班老师和课程老师都可以看
         if(user.getId()!= clazz.getUserId()&&user.getId()!=clazz.getCourse().getUserId()&&!relationshipClient.queryUserIdByClassId(clazz.getClassId()).contains(user.getId())){
             return new Result(ErrorCode.PERMISSION_DENIED,null,"您的无权查看" );
         }
@@ -135,29 +160,44 @@ public class ClassServiceImpl implements ClassService {
 
     }
 
+    /**
+     * 老师id查看分班
+     * @param request
+     * @return
+     */
     @Override
     public Result queryClassesByUserId(HttpServletRequest request) {
+        //登录校验
         if (Check.checkUser(request).getData()==null){
             return Check.checkUser(request);
         }
         User user =(User)Check.checkUser(request).getData();
-        System.out.println(user);
-        List<Class> classes = classMapper.queryClassByUserId(user.getId());
-        classes.stream().forEach(item->item.setUser(userClient.queryById(item.getUserId())));
-        //此方法要对老师
+
+        //此方法要对老师开放
         if(!user.getIdentity().equals(IdentityCode.TEACHER)){
             return new Result(ErrorCode.PERMISSION_DENIED,null,"您的无权查看" );
         }
+        //通过老师的id查带的班
+        List<Class> classes = classMapper.queryClassByUserId(user.getId());
+        classes.stream().forEach(item->item.setUser(userClient.queryById(item.getUserId())));
+
         return new Result(ErrorCode.OK,classes,"查询成功！");
     }
 
+    /**
+     * 学生查询class
+     * @param request
+     * @return
+     */
     @Override
     public Result queryClassesByStudentUserId(HttpServletRequest request) {
-        Object user1 = request.getSession().getAttribute("user");
-        if(user1==null){
-            return new Result(ErrorCode.NOT_LOGIN,null,"您的登录已过期");
+        //登录校验
+        if (Check.checkUser(request).getData()==null){
+            return Check.checkUser(request);
         }
-        User user =(User) user1;
+        User user =(User)Check.checkUser(request).getData();
+
+
 
         List<Integer> classIds= relationshipClient.queryClassIdByUserId(user.getId());
         List<Class> classes = classMapper.queryClassByClassIds(classIds);
